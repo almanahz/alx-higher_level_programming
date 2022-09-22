1580bbdff3a679d72a33f67f926106849c4a339b
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-09-22 14:50:10 +0000

    gootdl

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-import add_0
-num=add_0.add(1,2)
-print("{}".format(num, "d")
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))

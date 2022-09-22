b8f3434a3a0c01fb1c0d0dc1d4ec16e2a0021c5e
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-09-22 15:22:33 +0000

    test

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import calculator_1
+a = 10
+b = 5
+calculator_1.add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+calculator_1.sub(a, b)
+print("{:d} - {:d} = {:d}".format(a, b, add(a, b)))
+calculator_1.mul(a, b)
+print("{:d} * {:d} = {:d}".format(a, b, add(a, b)))
+calculator_1.div(a, b)
+print("{:d} / {:d} = {:d}".format(a, b, add(a, b)))

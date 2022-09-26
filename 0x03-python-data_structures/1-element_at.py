1b8fc6b32aeffe9ca8ce50e8ac24b01ebe961e50
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-09-26 11:06:20 +0000

    fisrs

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def element_at(my_list, idx):
+    for x in my_list:
+	if idx < 0:
+	    return none
+	if idx not in my_list:
+	    return none  
+	else:
+	    return my_list[idx]

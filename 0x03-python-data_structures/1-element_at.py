d49bb8bf2adc8fc81baaceef00581e341e0f4121
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2022-09-26 12:09:01 +0000

    bset

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= len(my_list):
-        return "none"
-    elif idx < 0:
-        return "none"
-    else:
+    if 0 <= idx < len(my_list):
         return my_list[idx]
+    else:
+        return "none"

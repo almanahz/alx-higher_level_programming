9e780c60ef87544b62c0cbfb515832428758ce4d
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-05-10 06:49:05 -0700

    commit1

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(0,10):
-    for j in range(i+1,10):
-        if i != 8:
-            print(f"{i}{j}", end=", ")
-        else:
-            print("89")
+for i in range(0, 8):
+    for j in range(i, 10):
+        if i != j:
+            print("{:d}{:d}, ".format(i, j), end='')
+print("89")

31c9737adfcbe17080c52c7c419576ba796432a6
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-05-14 03:14:24 -0700

    commit5

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,10 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    x = min(my_list)
-    else:
-        for i in my_list:
-            if i > x:
-                x = i
-        return x
+
+    max_no = min(my_list)
+    for i in my_list:
+        if i > max_no:
+            max_no = i
+
+    return max_no

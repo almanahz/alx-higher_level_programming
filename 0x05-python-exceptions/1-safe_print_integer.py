cfdac5fee73de0afa423378cd665230bd5bb56e0
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-05-29 10:49:40 -0700

    commit3

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    if my_list:
-        index = 0
-        count = 0
-        while (index < x):
-            try:
-                print("{:d}".format(my_list[index]), end="")
-                count+=1
-                index += 1
-            except (TypeError, ValueError):
-                index += 1
-                continue
-          
-        print("")
-    return count
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        return False

d178969614cfc733e703c2f0051bdcb05c57d94b
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-05-29 11:28:35 -0700

    commit2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+
+
+
     if my_list:
         index = 0
         count = 0
+
         while (index < x):
             try:
                 print("{:d}".format(my_list[index]), end="")
@@ -13,4 +17,5 @@ def safe_print_list_integers(my_list=[], x=0):
                 continue
           
         print("")
+
     return count

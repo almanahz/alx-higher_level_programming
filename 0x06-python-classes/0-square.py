4f6e2c83b0bcc8a207c0da86acd341f429c7b5c6
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-05-30 16:31:54 -0700

    done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,7 +6,4 @@ author: Abdulqudus Abdulsalam
 
 class Square:
     """This is an empty square which has no attribute"""
-    
-    def __init__(self)
-        """Initialize and accept argument for fields needed in
-        square"""
+    pass

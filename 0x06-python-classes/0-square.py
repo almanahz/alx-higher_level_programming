75692a2d02b0dee17bdd22977da1657ce5183878
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-05-30 14:29:42 -0700

    mess

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Square Class. This class does not do anything for the moment"""
 
 class Square:
-    def __init__(self):
+"""Square Class. This class does not do anything for the moment"""
         pass

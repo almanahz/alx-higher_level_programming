854ad752447cc1a47687443893a299e063316e1d
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-05-30 15:32:48 -0700

    trt

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-'''
+"""
 file_name:0-square.py
 description: contains a class square with no attributes
 author: Abdulqudus Abdulsalam
-'''
+"""
 
 class Square:
     """This is an empty square which has no attribute"""

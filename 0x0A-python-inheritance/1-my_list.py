5181847da13bc8ddec7d6eea6ccf398eba735523
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-11 04:32:36 -0700

    project2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This file contains a function that inherit from a class"""
+
+
+class MyLliist(list):
+    """This class uses inherited method to perform some
+    operations on a list"""
+
+    def print_sorted(self):
+        """This mwthod prints a sorted version of the instance"""
+        print(sorted(self.list))

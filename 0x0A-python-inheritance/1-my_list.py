cf7ff122e4cb24b25c0880d33dff93d3ee6fd818
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-12 05:37:51 -0700

    errorcorrected

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,10 +2,11 @@
 """This file contains a function that inherit from a class"""
 
 
-class MyLliist(list):
+class MyList(list):
     """This class uses inherited method to perform some
     operations on a list"""
 
     def print_sorted(self):
         """This mwthod prints a sorted version of the instance"""
-        print(sorted(self))
+        if issubclass(MyList, list):
+            print(sorted(self))

4ef3ee363fb117ca59a64026b5746d6785751b26
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-14 07:40:59 -0700

    project4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This file contains a function that appends to a text file"""
+import json
+
+
+def from_json_string(my_str):
+    """desc: This function performs some JSON operation
+    return: object represented by JSON string"""
+    return json.dumps(my_str)

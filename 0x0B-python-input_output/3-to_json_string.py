3354a87fce5aa398db3177607f065212a1ec3f6c
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-14 16:40:10 -0700

    project3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,6 +4,8 @@ JSON opertion"""
 
 
 from json import dumps
+
+
 def to_json_string(my_obj):
     """desc: This function performs some JSON operation
     return: JSON representation of an string"""

ae690957a9362d74bef886ad591b6be3feb974c8
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-14 16:36:24 -0700

    done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from json import dumps
 """This file contains a function that performs some
 JSON opertion"""
 
+
+from json import dumps
 
 def to_json_string(my_obj):
     """desc: This function performs some JSON operation

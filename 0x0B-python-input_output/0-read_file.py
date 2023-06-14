b9561338da0437c9cdc135400ab7245d470889fe
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-14 03:27:49 -0700

    project1

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     """This function reads a text file and returns
     the read file"""
     with open(filename, encoding='utf-8') as f:
-        return f.readlines()
+        print(f.readlines())
 

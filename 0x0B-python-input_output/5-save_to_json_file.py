1f9dd9800159bb16fc76488d5afb5f9d8e6cdb7b
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-14 16:42:53 -0700

    project5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,9 +3,11 @@
 JSON opertion"""
 
 from json import dump
+
+
 def save_to_json_file(my_obj, filename):
     """desc: This function performs some JSON operation
     return: none"""
     with open(filename, mode='w', encoding='utf-8') as f:
-        json_format = dump(my_obj)
+        json_format = dumps(my_obj)
         f.write(json_format)

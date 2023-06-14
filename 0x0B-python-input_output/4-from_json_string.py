f44e280d6566b8fd6fd834c8810a350e23a36928
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-14 15:44:34 -0700

    pull

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 from json import loads
+"""This file contains a function that performs some
+JSON opertion"""
 
 
 def from_json_string(my_str):
+    """desc: This function performs some JSON operation
+    return: object represented by JSON string"""
     return loads(my_str)

7ce4539a613156c8924a8e56bfaa4cacea7339e1
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-06-14 15:26:34 -0700

    project

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""This file contains a function that appends to a text file"""
-import json
+from json import loads
 
 
 def from_json_string(my_str):
-    """desc: This function performs some JSON operation
-    return: object represented by JSON string"""
-    return json.loads(my_str)
+    return loads(my_str)

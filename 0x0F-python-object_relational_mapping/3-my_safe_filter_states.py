0b81318a5458c28a2a96bbf304b09cb08ab23c97
Abdulqudus Abdulsalam <almanahz.aa@gmail.com>
2023-08-22 11:49:58 +0100

    A cript that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. But this time, write one that is safe from MySQL injections!

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     """
 
     connector = MySQLdb.connect(host="localhost", user=argv[1],
-                                port=3306,passwd=argv[2], db=argv[3])
+                                port=3306, passwd=argv[2], db=argv[3])
 
     with connector.cursor() as cur:
         cur.execute("""
